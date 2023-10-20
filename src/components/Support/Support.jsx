1196b7deb8a4e56ca23ad024a08bd5950709efdf
Supreet Kaur <supreet97@gmail.com>
2023-10-20 15:00:02 -0700

    added button container to style differently

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -6,7 +6,9 @@ function Support() {
         <section className="support">
             <h3 className="support__title">Support Those Impacted</h3>
             <span className="support__statement">Explore the different ways you can help those impacted by natural disasters within B.C.</span>
-            <Button modifier="--secondary" text='donate' className="button button--secondary support__button" />
+            <div className="support__button-container">
+                <Button modifier="--secondary" text='donate' />
+            </div>
         </section>
     )
 }
