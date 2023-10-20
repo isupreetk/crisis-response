e92073d06ba4e6d8ff652acc6f3b64197c0482b0
Supreet Kaur <supreet97@gmail.com>
2023-10-20 13:47:11 -0700

    Support component on landing page + Section Header component

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,13 +2,12 @@ import './LandingPage.scss';
 import HeroBanner from '../../components/HeroBanner/HeroBanner';
 import Button from '../../components/Button/Button';
 import Support from '../../components/Support/Support';
-
+import SectionHeader from '../../components/SectionHeader/SectionHeader';
 
 function LandingPage() {
     return (
         <>
             <HeroBanner />
-
 
             <div className='button-examples'>
                 <p>Button examples</p>
@@ -17,6 +16,9 @@ function LandingPage() {
                 <Button modifier="--secondary" text='donate' />
 
             </div>
+
+            <SectionHeader sectionHeading="Example Section Heading" />
+
             <Support />
 
         </>
