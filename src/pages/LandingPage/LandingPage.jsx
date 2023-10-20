77c8d439141743924e5ad6da0f65aab9abba8ef7
Supreet Kaur <supreet97@gmail.com>
2023-10-20 15:45:45 -0700

    merge conflicts resolved

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,6 @@
 import './LandingPage.scss';
 import HeroBanner from '../../components/HeroBanner/HeroBanner';
-import Button from '../../components/Button/Button';
+// import Button from '../../components/Button/Button';
 import DisasterList from '../../components/DisasterList/DisasterList';
 
 //data
@@ -9,7 +9,6 @@ import disasterData from '../../data/disasters.json';
 //components
 import Mission from '../../components/Mission/Mission';
 import Support from '../../components/Support/Support';
-import SectionHeader from '../../components/SectionHeader/SectionHeader';
 
 function LandingPage() {
     return (
@@ -17,15 +16,13 @@ function LandingPage() {
             <HeroBanner />
 
             <DisasterList disasterData={disasterData} />
+
             {/* <div className='button-examples'>
                 <p>Button examples</p>
                 <Button text="get help" />
                 <Button disabled={true} text='share your story' />
                 <Button modifier="--secondary" text='donate' />
-
             </div> */}
-
-            {/* <SectionHeader sectionHeading="Example Section Heading" /> */}
 
             <Mission />
             <Support />
