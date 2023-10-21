6b4e111c21ebf10d9e5da9a6509f2d7f6ed24ba0
Supreet Kaur <supreet97@gmail.com>
2023-10-21 01:19:33 -0700

    completed disaster detail hero component

--- src/components/DisasterDetailHero/DisasterDetailHero.jsx
+++ src/components/DisasterDetailHero/DisasterDetailHero.jsx
@@ -1,6 +1,7 @@
 import "./DisasterDetailHero.scss";
 import Map from "../../assets/images/map-mcdougall-wildfire.svg";
 import Button from "../Button/Button";
+import AlternateSectionHeader from "../AlternateSectionHeader/AlternateSectionHeader";
 
 function DisasterDetailHero({ selectedDisaster }) {
 
@@ -8,27 +9,27 @@ function DisasterDetailHero({ selectedDisaster }) {
 
     return (
         <section className="disaster-detail-hero">
-            <h3>{selectedDisaster?.name}</h3>
-            <h5>{selectedDisaster?.location}</h5>
+            <h3 className="disaster-detail-hero__disaster-name">{selectedDisaster?.name}</h3>
+            <AlternateSectionHeader alternateSectionHeading={selectedDisaster?.location} />
             <img src={Map} alt="location on map" />
             {/* <img src={selectedDisaster?.mapPhoto} alt="location on map" /> */}
 
             <div className="disaster-detail-hero__about-section">
                 <span><strong>Started on: </strong>{selectedDisaster?.startDateText}</span>
-                <h3>Impact</h3>
+                <AlternateSectionHeader alternateSectionHeading="Impact" />
             </div>
             <div className="disaster-detail-hero__impact-section">
                 <div className="disaster-detail-hero__impact-1">
-                    <p>{selectedDisaster?.impacts[0].value}</p>
-                    <p>{selectedDisaster?.impacts[0].metric}</p>
+                    <p className="disaster-detail-hero__impact-value">{selectedDisaster?.impacts[0].value}</p>
+                    <p className="disaster-detail-hero__impact-metric">{selectedDisaster?.impacts[0].metric}</p>
                 </div>
                 <div className="disaster-detail-hero__impact-1">
-                    <p>{selectedDisaster?.impacts[1].value}</p>
-                    <p>{selectedDisaster?.impacts[1].metric}</p>
+                    <p className="disaster-detail-hero__impact-value">{selectedDisaster?.impacts[1].value}</p>
+                    <p className="disaster-detail-hero__impact-metric">{selectedDisaster?.impacts[1].metric}</p>
                 </div>
                 <div className="disaster-detail-hero__impact-1">
-                    <p>{selectedDisaster?.impacts[2].value}</p>
-                    <p>{selectedDisaster?.impacts[2].metric}</p>
+                    <p className="disaster-detail-hero__impact-value">{selectedDisaster?.impacts[2].value}</p>
+                    <p className="disaster-detail-hero__impact-metric">{selectedDisaster?.impacts[2].metric}</p>
                 </div>
             </div>
             <div className="disaster-detail-hero__button">
