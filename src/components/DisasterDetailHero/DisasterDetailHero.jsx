47f8af5141a9a7f39b60bbed41042a5393d299a2
Supreet Kaur <supreet97@gmail.com>
2023-10-21 01:38:34 -0700

    added disaster status to map image

--- src/components/DisasterDetailHero/DisasterDetailHero.jsx
+++ src/components/DisasterDetailHero/DisasterDetailHero.jsx
@@ -2,6 +2,8 @@ import "./DisasterDetailHero.scss";
 import Map from "../../assets/images/map-mcdougall-wildfire.svg";
 import Button from "../Button/Button";
 import AlternateSectionHeader from "../AlternateSectionHeader/AlternateSectionHeader";
+import ongoing from "../../assets/icons/ongoing.svg";
+import recovery from "../../assets/icons/recovery.svg";
 
 function DisasterDetailHero({ selectedDisaster }) {
 
@@ -11,11 +13,24 @@ function DisasterDetailHero({ selectedDisaster }) {
         <section className="disaster-detail-hero">
             <h3 className="disaster-detail-hero__disaster-name">{selectedDisaster?.name}</h3>
             <AlternateSectionHeader alternateSectionHeading={selectedDisaster?.location} />
-            <img src={Map} alt="location on map" />
-            {/* <img src={selectedDisaster?.mapPhoto} alt="location on map" /> */}
-
+            <div className="disaster-detail-hero__map-container">
+                <img src={Map} alt="location on map" />
+                {selectedDisaster?.status == "ongoing" && <img src={ongoing} alt="ongoing" className="disaster-detail-hero__map-disaster-status" />}
+                {selectedDisaster?.status == "recovery" && <img src={recovery} alt="recovery" className="disaster-detail-hero__map-disaster-status" />}
+            </div>
             <div className="disaster-detail-hero__about-section">
-                <span><strong>Started on: </strong>{selectedDisaster?.startDateText}</span>
+                {selectedDisaster?.status == "ongoing"
+                            ? <p>
+                                <span>
+                                    <strong>
+                                    Started on: 
+                                    </strong>
+                                {` ${selectedDisaster?.startDateText}`}
+                                </span>
+                            </p>
+                            : <p>{selectedDisaster?.startDateText} - {selectedDisaster?.endDateText}</p>
+
+                        }  
                 <AlternateSectionHeader alternateSectionHeading="Impact" />
             </div>
             <div className="disaster-detail-hero__impact-section">
