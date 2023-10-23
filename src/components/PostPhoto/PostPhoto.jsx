9e57781e3574742a0428295f28c189024ca3b9f2
anagelberg <amy.nagelberg@gmail.com>
2023-10-22 22:16:49 -0700

    add photo modal:

--- src/components/PostPhoto/PostPhoto.jsx
+++ src/components/PostPhoto/PostPhoto.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
+import PhotoModal from '../PhotoModal/PhotoModal';
 import './PostPhoto.scss'
 
 function PostPhoto({ photoUrl }) {
+
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <div className='post-photo'>
             {photoUrl
                 && <
                     img src={photoUrl}
+                    onClick={() => setShowModal(true)}
                     className="post-photo__img"
                     alt="photo of disaster"
                 />
             }
+
+            <PhotoModal show={showModal}
+                photoUrl={photoUrl}
+                setShow={setShowModal} />
+
         </div>
     )
 };
